28c0a64c31baaf0d7608db33bef8c3a8d277d68f
Amnon Skladman <amnon.skladman@gmail.com>
2018-07-23 17:20:12 -0400

    Initial commit

---
+++ form.php
@@ -0,0 +1,22 @@
+<?php
+    // START OF SUBMISSION FORM
+    if (isset($_POST['formName'])) {  
+        $formName = $_POST['formName'];
+    };
+
+    if (isset($_POST['formEmail'])) {
+        $formEmail = $_POST['formEmail'];
+    };
+
+    if (isset($_POST['formMessage'])) {
+        $formMessage = $_POST['formMessage'];
+    };
+
+    if(isset($formName) && isset($formEmail) && isset($formMessage)) {
+        mail('thewoolifox@gmail.com', 'Message from '.$formName.' at '.$formEmail, $formMessage);
+        echo ('{"success": "true"}');
+    } else {
+        echo ('{"success": "false"}');
+    };
+    // END OF SUBMISSION FORM
+?>
